399b256e2e9a813dafc54c2701c497d1bc356171
lijing <1292640216@qq.com>
2018-03-23 18:13:59 +0800

    init

---
+++ src/Main.ts
@@ -0,0 +1,9 @@
+class Main extends egret.DisplayObjectContainer {
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage(event: egret.Event) {
+    }
+}
